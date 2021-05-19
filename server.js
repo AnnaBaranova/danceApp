0677572db24ff36f8c4ea73e069d228fca38349a
Anna Baranova <asbaranova1@gmail.com>
2021-05-19 19:39:53 -0400

    update server

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// mongoDB connection
+require('./config/database');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
