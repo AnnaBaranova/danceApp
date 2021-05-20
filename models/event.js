8d8a4420c001e903eec3e89682900b3d0df290df
Anna Baranova <asbaranova1@gmail.com>
2021-05-19 22:38:48 -0400

    add event model and comment embedding model

---
+++ models/event.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+    {
+        text: String,
+        userId: {
+            type: Schema.Types.ObjectId, ref: 'User'
+        },
+        userName: String,
+    }, {
+    timestamps: true,
+}
+);
+
+const eventSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        host: {
+            type: Schema.Types.ObjectId, ref: 'User'
+        },
+        date: Date,
+        place: String,
+        guestLimit: {
+            type: Number,
+            min: 1,
+            default: 6
+        },
+        details: String,
+        attendees: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+        comments: [commentSchema],
+    },
+    {
+        timestamps: true,
+    }
+
+)
+
+module.exports = mongoose.model('User', userSchema);
