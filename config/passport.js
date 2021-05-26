217b0e393bf8b01213f8276b2359a1fef6ab5154
Anna Baranova <asbaranova1@gmail.com>
2021-05-26 19:50:05 -0400

    add server address to env

--- config/passport.js
+++ config/passport.js
@@ -1,49 +1,48 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const User = require ('../models/user');
+const User = require('../models/user');
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  },
-  function(accessToken, refreshToken, googleProfile, cb) {
-    
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
+},
+  function (accessToken, refreshToken, googleProfile, cb) {
+
     User.findOne({ googleId: googleProfile.id })
-    .then((user) => {
-      if (user) {
-        if (user.avatar) {
-        //   return cb(null, user);
-          return user;
+      .then((user) => {
+        if (user) {
+          if (user.avatar) {
+            return user;
+          }
+          user.avatar = googleProfile.photos[0].value;
+          return user.save()
         }
-        user.avatar = googleProfile.photos[0].value;
-        return user.save()
-      }
 
-      const newUser = new User({
-        name: googleProfile.displayName,
-        email: googleProfile.emails[0].value,
-        googleId: googleProfile.id,
-        avatar: googleProfile.photos[0].value,
-      });   
+        const newUser = new User({
+          name: googleProfile.displayName,
+          email: googleProfile.emails[0].value,
+          googleId: googleProfile.id,
+          avatar: googleProfile.photos[0].value,
+        });
 
-      return newUser.save();
-    })
-    .then((user) => cb(null, user))
-    .catch((err) => cb(err));
-}
+        return newUser.save();
+      })
+      .then((user) => cb(null, user))
+      .catch((err) => cb(err));
+  }
 )
 );
 
 passport.serializeUser(function (user, done) {
-done(null, user.id);
+  done(null, user.id);
 });
 
 
 passport.deserializeUser(function (id, done) {
-User.findById(id)
-.then((user) => {
-  done(null, user);
-})
-.catch((err) => done(err));
+  User.findById(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => done(err));
 });
